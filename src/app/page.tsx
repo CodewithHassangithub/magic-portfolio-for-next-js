bc1fbd5e463e3ab09982c2a20981a87c31aecd3b
Ali Hassan <141361380+CodewithHassangithub@users.noreply.github.com>
2025-04-07 02:48:22 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import { getPosts } from "@/app/utils/utils";
-import { Column } from "@/once-ui/components";
+import React from "react";
+
+import { Heading, Flex, Text, Button, Avatar, RevealFx, Arrow, Column } from "@/once-ui/components";
 import { Projects } from "@/components/work/Projects";
-import { baseURL } from "@/app/resources";
-import { person, work } from "@/app/resources/content";
+
+import { baseURL, routes } from "@/app/resources";
+import { home, about, person, newsletter } from "@/app/resources/content";
+import { Mailchimp } from "@/components";
+import { Posts } from "@/components/blog/Posts";
 
 export async function generateMetadata() {
-  const title = work.title;
-  const description = work.description;
+  const title = home.title;
+  const description = home.description;
   const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
 
   return {
@@ -16,7 +20,7 @@ export async function generateMetadata() {
       title,
       description,
       type: "website",
-      url: `https://${baseURL}/work/`,
+      url: `https://${baseURL}`,
       images: [
         {
           url: ogImage,
@@ -33,37 +37,83 @@ export async function generateMetadata() {
   };
 }
 
-export default function Work() {
-  let allProjects = getPosts(["src", "app", "work", "projects"]);
-
+export default function Home() {
   return (
-    <Column maxWidth="m">
+    <Column maxWidth="m" gap="xl" horizontal="center">
       <script
         type="application/ld+json"
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
             "@context": "https://schema.org",
-            "@type": "CollectionPage",
-            headline: work.title,
-            description: work.description,
-            url: `https://${baseURL}/projects`,
-            image: `${baseURL}/og?title=Design%20Projects`,
-            author: {
+            "@type": "WebPage",
+            name: home.title,
+            description: home.description,
+            url: `https://${baseURL}`,
+            image: `${baseURL}/og?title=${encodeURIComponent(home.title)}`,
+            publisher: {
               "@type": "Person",
               name: person.name,
+              image: {
+                "@type": "ImageObject",
+                url: `${baseURL}${person.avatar}`,
+              },
             },
-            hasPart: allProjects.map((project) => ({
-              "@type": "CreativeWork",
-              headline: project.metadata.title,
-              description: project.metadata.summary,
-              url: `https://${baseURL}/projects/${project.slug}`,
-              image: `${baseURL}/${project.metadata.image}`,
-            })),
           }),
         }}
       />
-      <Projects />
+      <Column fillWidth paddingY="l" gap="m">
+        <Column maxWidth="s">
+          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
+            <Heading wrap="balance" variant="display-strong-l">
+              {home.headline}
+            </Heading>
+          </RevealFx>
+          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
+            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
+              {home.subline}
+            </Text>
+          </RevealFx>
+          <RevealFx translateY="12" delay={0.4} horizontal="start">
+            <Button
+              id="about"
+              data-border="rounded"
+              href="/about"
+              variant="secondary"
+              size="m"
+              arrowIcon
+            >
+              <Flex gap="8" vertical="center">
+                {about.avatar.display && (
+                  <Avatar
+                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
+                    src={person.avatar}
+                    size="m"
+                  />
+                )}
+                {about.title}
+              </Flex>
+            </Button>
+          </RevealFx>
+        </Column>
+      </Column>
+      <RevealFx translateY="16" delay={0.6}>
+        <Projects range={[1, 1]} />
+      </RevealFx>
+      {routes["/blog"] && (
+        <Flex fillWidth gap="24" mobileDirection="column">
+          <Flex flex={1} paddingLeft="l">
+            <Heading as="h2" variant="display-strong-xs" wrap="balance">
+              Latest from the blog
+            </Heading>
+          </Flex>
+          <Flex flex={3} paddingX="20">
+            <Posts range={[1, 2]} columns="2" />
+          </Flex>
+        </Flex>
+      )}
+      <Projects range={[2]} />
+      {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
 }
